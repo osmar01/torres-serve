54d6f4f10ce790228e9dfb165e4cdc087e51ad20
Osmar Junio Evangelista Frota <osmar.frota@ITBAM.ORG.BR>
2019-09-23 08:11:48 -0400

    first commit

---
+++ app/Controllers/Http/PropertyController.js
@@ -0,0 +1,89 @@
+'use strict'
+
+const Property = use('App/Models/Property')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with properties
+ */
+class PropertyController {
+  /**
+   * Show a list of all properties.
+   * GET properties
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index({ request }) {
+
+    const { latitude, longitude } = request.all()
+
+    const properties = Property.query()
+      .with('images')
+      .nearBy(latitude, longitude, 10)
+      .fetch()
+
+    return properties
+  }
+
+  async store({ auth, request }) {
+
+    const { id } = auth.user
+    const data = request.only([
+      'title',
+      'address',
+      'latitude',
+      'longitude',
+      'price'
+    ])
+
+    const property = await Property.create({ ...data, user_id: id })
+
+    return property
+  }
+
+  async show({ params }) {
+    const property = await Property.findOrFail(params.id)
+
+    await property.load('images')
+
+    return property
+  }
+
+  async update({ params, request }) {
+
+    const property = await Property.findOrFail(params.id)
+
+    const data = request.only([
+      'title',
+      'address',
+      'latitude',
+      'longitude',
+      'price'
+    ])
+
+    property.merge(data)
+
+    await property.save()
+
+    return property
+  }
+
+  async destroy({ params, auth, response }) {
+
+    const property = await Property.findOrFail(params.id)
+
+    if (property.user_id !== auth.user.id) {
+      return response.status(401).send({ error: 'Not authorized' })
+    }
+
+    await property.delete()
+  }
+}
+
+module.exports = PropertyController
